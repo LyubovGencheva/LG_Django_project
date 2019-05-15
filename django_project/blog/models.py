173da288da70f176f24ce3215619a9705558e320
Lyubov Gencheva <46396752+LyubovGencheva@users.noreply.github.com>
2019-05-16 00:54:27 +0300

    blog/models: class Post; set-up Posts to admin page; set-up blog/views->homepage to loop through the post objects in templates/blog/home update the rendered date format

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)    # if no specific date was added, it sets that to today
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # if a user is deleted, then all their posts would be deleted
+                                                                # ForeignKey relation means that an Author(User) can have many posts, but a post can only have one Author
+
+    def __str__(self):
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . models import Post
+
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Lyubov',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'May 13, 2019'
-     },
-    {
-        'author': 'Lyubov',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 14, 2019'
-    },
-    {
-        'author': 'Maria',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'May 15, 2019'
-    },
-]
-
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
-
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})  #the last variable is the context dictionary to be passed to the about template
