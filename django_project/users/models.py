7d35141f79a5543b1437ef3e4072fc0aa47c03c8
Lyubov Gencheva <46396752+LyubovGencheva@users.noreply.github.com>
2019-05-18 17:33:54 +0300

    Updated Profile model to fix a force_insert error, preventing the user creation from the admin panel: in users/models.py: class Profile, when overriding the save() method: def save(self, *args, **kwargs) (added *args, **kwargs)

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     # Override the save method to resize the profile pic before saving it.
-    def save(self):
+    def save(self, *args, **kwargs):
         super().save()                              # running the save method on the parent class
                                                     # will save the uploaded image just as it is
         img = Image.open(self.image.path)
